24722969cddd9f0553c1f147ff92e196f053cacf
MistyClawn <acidclaw11@gmail.com>
2022-05-25 13:54:23 +0900

    link to mainactivity
    add login register btn

--- app/src/main/java/com/cookandroid/mathcore/MainActivity.java
+++ app/src/main/java/com/cookandroid/mathcore/MainActivity.java
@@ -2,7 +2,10 @@ package com.cookandroid.mathcore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goto_login_btn = findViewById(R.id.goto_login_btn);
+
+        Button goto_regist_btn = findViewById(R.id.goto_register_btn);
+
+        goto_login_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                Intent myIntent = new Intent(MainActivity.this, LoginActivity.class);
+                //go to login
+                startActivity(myIntent);
+                finish();
+                //finish mainactivity
+            }
+        });
+
+        goto_regist_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v){
+                Intent myIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                //go to regist
+                startActivity(myIntent);
+                //finish();
+                //finish mainactivity
+            }
+        });
 
     }
 }
