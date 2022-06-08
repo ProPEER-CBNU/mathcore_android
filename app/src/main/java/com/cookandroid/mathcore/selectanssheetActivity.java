622808b0a527080b6479dbec94f4a9d7559b69da
kgu011016 <kgu011016@gamil.com>
2022-06-08 23:21:49 +0900

    22-06-08 selectanssheet 변경

--- app/src/main/java/com/cookandroid/mathcore/selectanssheetActivity.java
+++ app/src/main/java/com/cookandroid/mathcore/selectanssheetActivity.java
@@ -2,10 +2,12 @@ package com.cookandroid.mathcore;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.IdRes;
@@ -15,15 +17,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class selectanssheetActivity extends AppCompatActivity {
     Button Scoring_btn;
-    RadioButton r1_btn, r2_btn, r3_btn;
-    RadioGroup radioG;
+    TextView textv, r1_btn, r2_btn, r3_btn, r4_btn;
 
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selectanssheet);
 
         Scoring_btn=(Button) findViewById(R.id.Scoring_btn);
-        radioG=(RadioGroup) findViewById(R.id.radioG);
+        textv=(TextView)findViewById(R.id.textv);
+        r1_btn=(TextView)findViewById(R.id.r1_btn);
+        r2_btn=(TextView)findViewById(R.id.r2_btn);
+        r3_btn=(TextView)findViewById(R.id.r3_btn);
+        r4_btn=(TextView)findViewById(R.id.r4_btn);
 
         Scoring_btn.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -32,25 +37,44 @@ public class selectanssheetActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        radioG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+        r1_btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId){
-                    case R.id.r1_btn:
-                        Toast.makeText(getApplicationContext(),"Math note 1-1",Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.r2_btn:
-                        Toast.makeText(getApplicationContext(), "Ssen elementry", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.r3_btn:
-                        Toast.makeText(getApplicationContext(),"Middle test",Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.r4_btn:
-                        Toast.makeText(getApplicationContext(),"1-1 다항식의 연산",Toast.LENGTH_SHORT).show();
-                        break;
-                }
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Mathcore 1-1",Toast.LENGTH_SHORT).show();
+                textv.setText("Mathcore 1-1");
             }
         });
+        r2_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Ssen elementry",Toast.LENGTH_SHORT).show();
+                textv.setText("Ssen elementry");
+            }
+        });
+
+        r3_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Middle test",Toast.LENGTH_SHORT).show();
+                textv.setText("Middle test");
+            }
+        });
+
+        r4_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"1-1 다항식의 연산",Toast.LENGTH_SHORT).show();
+                textv.setText("1-1 다항식의 연산");
+            }
+        });
+
+
+
+
+
+
+
 
 
     }
